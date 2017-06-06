cdb29fbf56e625426e374669dfa196170f6afbb1
Kachroo-Lab <ashroo6@gmail.com>
2017-06-06 14:07:03 -0500

    t

--- classes/SDS348/2017_spring/worksheets/class29_3KAS.py
+++
@@ -1,8 +0,0 @@
-# Problem 1
-cmd.fetch("3KAS")
-cmd.hide("all")
-cmd.show("cartoon", "all")
-cmd.select("chain_A", "chain A")
-cmd.color("blue", "(chain_A)")
-cmd.select("chain_B", "chain B")
-cmd.color("green", "(chain_B)")
